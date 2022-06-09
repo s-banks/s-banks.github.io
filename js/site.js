0582407a3d34cd3849f7a33492e5f44c7529a8bb
Shane Banks <shane.banks02@gmail.com>
2022-06-09 16:53:42 -0500

    Added basic table and API for github repos

--- js/site.js
+++ js/site.js
@@ -1,5 +1,15 @@
 "use strict";
 (function () {
+    const URL = `https://api.github.com/users/s-banks/repos`
+    fetch(URL, {headers: {'Authorization': `token ${GITHUB_API_KEY}`}})
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            // document.querySelector('#user').innerHTML = `${data['0']['actor'].login}`,
+            //     document.querySelector('#last-push').innerHTML = `${data['0']['created_at']}`,
+            //     document.querySelector('#repo').innerHTML = `${data['0']['repo'].name}`
+        })
+        .catch(error => console.error(error));
 
 
 }())
