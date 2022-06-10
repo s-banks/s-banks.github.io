34cc1cdc872fa4ca97e32251a9ab0b8bcc643702
Shane Banks <shane.banks02@gmail.com>
2022-06-10 09:50:40 -0500

    Disabled API script till more secure way to handle is found

--- js/site.js
+++ js/site.js
@@ -1,34 +1,30 @@
 "use strict";
 (function () {
-    const URL = `https://api.github.com/users/s-banks/repos`
-    fetch(URL, {headers: {'Authorization': `token ${GITHUB_API_KEY}`}})
-        .then((resp) => resp.json())
-        .then((repos) => {
-            buildRepos(repos);
-        })
-        .catch(error => console.error(error));
-
-
-    function buildRepos(repos) {
-        $("#repos").empty()
-        repos.forEach(repo => {
-            let localTimeandDate = new Date(`${repo.pushed_at}`).toLocaleString();
-            let datePushed = localTimeandDate.slice(0, -12)
-                $("#repos").append(`
-         <tr>
-             <th scope="row">1</th>
-             <td><a href="${repo.html_url}" target="_blank">${repo.name}</a></td>
-             <td>${repo.language}</td>
-             <td>${repo.description}</td>
-             <td>${datePushed}</td>
-         </tr>`)
-        })
-    }
-
-
-
-
-
-
+    //script deactivated until I learn how to secure an API call externally
+    // const URL = `https://api.github.com/users/s-banks/repos`
+    // fetch(URL, {headers: {'Authorization': `token ${GITHUB_API_KEY}`}})
+    //     .then((resp) => resp.json())
+    //     .then((repos) => {
+    //         buildRepos(repos);
+    //     })
+    //     .catch(error => console.error(error));
+    //
+    //
+    // function buildRepos(repos) {
+    //     $("#repos").empty()
+    //     repos.forEach(repo => {
+    //         let localTimeandDate = new Date(`${repo.pushed_at}`).toLocaleString();
+    //         let datePushed = localTimeandDate.slice(0, -12)
+    //         let lineNum = (repos.indexOf(repo) + 1)
+    //             $("#repos").append(`
+    //      <tr>
+    //          <th scope="row">${lineNum}</th>
+    //          <td><a href="${repo.html_url}" target="_blank">${repo.name}</a></td>
+    //          <td>${repo.language}</td>
+    //          <td>${repo.description}</td>
+    //          <td>${datePushed}</td>
+    //      </tr>`)
+    //     })
+    // }
 
 }())
