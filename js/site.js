e258a360b8681a5414dbe9b2d596cd1a5ab04e7f
s-banks <shroud2@hotmail.com>
2022-06-09 20:49:38 -0500

    API script updated to build repo table

--- js/site.js
+++ js/site.js
@@ -3,13 +3,32 @@
     const URL = `https://api.github.com/users/s-banks/repos`
     fetch(URL, {headers: {'Authorization': `token ${GITHUB_API_KEY}`}})
         .then((resp) => resp.json())
-        .then((data) => {
-            console.log(data)
-            // document.querySelector('#user').innerHTML = `${data['0']['actor'].login}`,
-            //     document.querySelector('#last-push').innerHTML = `${data['0']['created_at']}`,
-            //     document.querySelector('#repo').innerHTML = `${data['0']['repo'].name}`
+        .then((repos) => {
+            buildRepos(repos);
         })
         .catch(error => console.error(error));
+
+
+    function buildRepos(repos) {
+        $("#repos").empty()
+        repos.forEach(repo => {
+            let localTimeandDate = new Date(`${repo.pushed_at}`).toLocaleString();
+            let datePushed = localTimeandDate.slice(0, -12)
+                $("#repos").append(`
+         <tr>
+             <th scope="row">1</th>
+             <td><a href="${repo.html_url}" target="_blank">${repo.name}</a></td>
+             <td>${repo.language}</td>
+             <td>${repo.description}</td>
+             <td>${datePushed}</td>
+         </tr>`)
+        })
+    }
+
+
+
+
+
 
 
 }())
